1e1ae26648f998eda0fb6d5eccb3ca15b297c29b
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-11-08 23:08:43 -0800

    Capstone Site - Added more forms elements

--- Class Work/Capstone Bootstrap Site/src/Components/SignUpForm.js
+++ Class Work/Capstone Bootstrap Site/src/Components/SignUpForm.js
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
 function SignUpForm(props) {
     return (
@@ -37,6 +38,32 @@ function SignUpForm(props) {
                         <FormControl id="inlineFormInputGroup" placeholder="Username" />
                     </InputGroup>
 
+
+                    {/* Added more inputs to satisfy homework requirements */}
+                    <Form.Group controlId="formGridAddress2">
+                        <Form.Label>Address 2</Form.Label>
+                        <Form.Control placeholder="Apartment, studio, or floor" />
+                    </Form.Group>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control as="select" defaultValue="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridZip">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form.Row>
                 </Form>
 
             </Modal.Body>
